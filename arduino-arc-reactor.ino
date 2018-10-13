055562158f4174a59df525cece824811f13ff9b5
James Tanner <admin@codingentity.co.uk>
2018-10-13 20:59:07 +0100

    renaming main file. adding some debug.

--- arduino-arc-reactor.ino
+++ arduino-arc-reactor.ino
@@ -3,8 +3,9 @@
 #include "pulse.h"
 #include "spinner.h"
 #include "decay.h"
+#include "MemoryFree.h"
 
-const uint8_t modeCount = 6;
+const uint8_t modeCount = 8;
 uint8_t mode;
 //*******************************************************************************************
 //                                arduino functions
@@ -17,10 +18,25 @@ void setup() {
   //initilaize the pixels
 
   //increment the counter and loop around if needed
-  mode = EEPROM.read(0);
-  EEPROM.write(0,(mode+1)%modeCount);
-  //mode = 7;
+  //mode = EEPROM.read(0);
+  //EEPROM.write(0,(mode+1)%modeCount);
+  mode = 7;
+
   PixelPtr pixel = new Adafruit_NeoPixel(PIXELCOUNT,PIXELPIN);
+#define DEBUG
+#ifdef DEBUG
+for (int i = 0 ; i < mode ; i ++)
+{
+  pixel->setPixelColor(i,0,40,0);
+  delay(100);
+}
+  pixel->begin();
+  pixel->show();
+  Serial.begin(38400);
+ while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+#endif
   if(mode == 0)
   {
     currentMode = new Static(pixel);
@@ -51,15 +67,20 @@ void setup() {
   }
   else if (mode == 7)
   {
-      //currentMode = new Decay(pixel);
+      currentMode = new Decay(pixel);
   }
   //run specific setup for the mode.
   currentMode->modeSetup();
   delay(100);
-  Serial.begin(9600);
-
+  Serial.print("reboot : mode ");
+  Serial.println(mode);
 }
 
 void loop() {
+  
+  //Serial.println("Hello World");
+  delay(10);
   currentMode->runMode();
+   //Serial.print("freeMemory()=");
+    //Serial.println(freeMemory());
 }
